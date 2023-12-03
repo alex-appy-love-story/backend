338ac0f6dcda8a2c339d2b493b040fcadac63261
Appy <106456906+TheGreatWaves@users.noreply.github.com>
2023-12-03 20:48:41 +0700

    fix: corrected payload, removed binary

--- handler/order.go
+++ handler/order.go
@@ -11,30 +11,24 @@ import (
 	"github.com/hibiken/asynq"
 )
 
-type OrderInfo struct {
-    Username string `json:"username"`
-	TokenID uint `json:"token_id"`
-	Amount  uint `json:"amount"`
-}
-
 func Create(w http.ResponseWriter, r *http.Request) {
-    fmt.Println("Got request")
+	fmt.Println("Got request")
 	ctx := tasks.GetContext(r.Context())
-    orderInfo := &model.OrderInfo{}
+	orderInfo := &model.OrderInfo{}
 
-    if err := json.NewDecoder(r.Body).Decode(&orderInfo); err != nil {
-        return
-    }
+	if err := json.NewDecoder(r.Body).Decode(&orderInfo); err != nil {
+		return
+	}
 
-    task, err := tasks.NewStartOrderTask(*orderInfo)
-    if err != nil {
-        log.Fatalf("could not create task: %v", err)
-    }
-    fmt.Println("ENQUEING")
-    info, err := ctx.AsynqClient.Enqueue(task, asynq.Queue("order"), asynq.MaxRetry(0))
-    if err != nil {
-        log.Fatalf("could not enqueue task: %v", err)
-    }
-    log.Printf("enqueued task: id=%s queue=%s", info.ID, info.Queue)   
+	task, err := tasks.NewStartOrderTask(*orderInfo)
+	if err != nil {
+		log.Fatalf("could not create task: %v", err)
+	}
+	fmt.Println("ENQUEING")
+	info, err := ctx.AsynqClient.Enqueue(task, asynq.Queue("order"), asynq.MaxRetry(0))
+	if err != nil {
+		log.Fatalf("could not enqueue task: %v", err)
+	}
+	log.Printf("enqueued task: id=%s queue=%s", info.ID, info.Queue)
 
 }
--- model/order.go
+++ model/order.go
@@ -1,7 +1,7 @@
 package model
 
 type OrderInfo struct {
-	UserID  uint `json:"user_id"`
-	TokenID uint `json:"token_id"`
-	Amount  uint `json:"amount"`
+	Username string `json:"username"`
+	TokenID  uint   `json:"token_id"`
+	Amount   uint   `json:"amount"`
 }
